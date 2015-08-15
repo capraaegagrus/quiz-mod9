1de10edfba9736ee4a6f96990175d7daad1c7f55
capraaegagrus <manuelguimarey@gmail.com>
2015-08-15 08:40:25 +0200

    Insertar comentario

---
+++ models/comment.js
@@ -0,0 +1,13 @@
+//Deficición do modelo de Comment con validación
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define(
+    'Comment',
+    { texto: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: {msg: "-> Falta Comentario"}}
+      }
+    }
+  );
+}
+
